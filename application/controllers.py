9586ecf480c412685928b38344d93b2e76cda1df
harishsahadev <harishsahadev@gmail.com>
2022-03-15 16:23:29 +0530

    Add Tracker

--- application/controllers.py
+++ application/controllers.py
@@ -1,6 +1,6 @@
 from flask import redirect, render_template, request, url_for, flash, session
 from flask import current_app as app
-from application.models import User, Tracker_type, Tracker
+from application.models import *
 from application.forms import RegistrationForm, LoginForm
 from application.database import db
 
@@ -13,11 +13,11 @@ def index():
     if form.validate_on_submit():
         username = request.form["username"]
         user = User.query.filter_by(username=username).first()
-        session["user"] = user.userid
         if (user == None) or (user.password != request.form["password"]):
             flash("Login unsuccessful! Please check username or password.", category='danger')
             return render_template('login.html', title="Login", form=form)
         else:
+            session["user"] = user.userid
             return redirect(url_for('dashboard', userid=user.userid))
     
     if "user" in session:
@@ -47,25 +47,52 @@ def register():
 def dashboard(userid):
     if "user" not in session:
         return redirect(url_for("index"))
-    user = User.query.filter_by(userid=userid).first()
-    tracker = Tracker.query.filter_by(userid=userid).all()
-
-    return render_template('dashboard.html', title="Dashboard", username=user.fname, userid=userid, tracker=tracker)
+    user = User.query.filter_by(userid=session["user"]).first()
+    trackers = Tracker.query.filter_by(userid=userid).all()
+    print(trackers)
+    return render_template('dashboard.html', title="Dashboard", username=user.fname, userid=userid, trackers=trackers)
 
 tracker_type = ["Numeric", "Muliple Choice", "Time Duration", "Boolean"]
 
-@app.route("/dashboard/<int:userid>/create", methods=["GET","POST"])
-def create_tracker(userid):
+@app.route("/dashboard/create", methods=["GET","POST"])
+def create_tracker():
     if "user" not in session:
         return redirect(url_for("index"))
-    user = User.query.filter_by(userid=userid).first()
+    user = User.query.filter_by(userid=session["user"]).first()
     
     if request.method == "POST":
-        type = request.form.get('tracker_type')
-        if tracker_type[1] == type:
-            return render_template("multiple_choice.html", title="Add choices", username=user.fname, userid=userid)
+        tracker = Tracker.query.filter_by(userid=session["user"]).first()
+        if tracker.trackername == request.form["name"]:
+            flash('Tracker Already Exist!', category='danger')
+        else:
+            tracker = Tracker(trackername=request.form["name"], description=request.form["description"], type=request.form.get('tracker_type'), userid=session["user"])
+            db.session.add(tracker)
+            db.session.commit()
 
-    return render_template('create_tracker.html', title="Create Tracker", username=user.fname, userid=userid, tracker_type=tracker_type)
+            if tracker_type[1] == request.form.get('tracker_type'):
+                return redirect(url_for('multiple_choice'))
+        #print(session["user"])
+        return redirect(url_for('dashboard', userid=session["user"]))
+
+    return render_template('create_tracker.html', title="Create Tracker", username=user.fname, tracker_type=tracker_type)
+
+@app.route("/dashboard/multiple_choice", methods=["GET","POST"])
+def multiple_choice():
+    if "user" not in session:
+        return redirect(url_for("index"))
+    user = User.query.filter_by(userid=session["user"]).first()
+
+    if request.method == "POST":
+        tracker = Tracker.query.filter_by(userid=session["user"]).first()
+        choices = request.form["choices"].split(",")
+        for item in choices:
+            mcq = MultipleChoice(trackerid=tracker.trackerid, choices=item)
+            db.session.add(mcq)
+            db.session.commit()
+
+        return redirect(url_for('dashboard'), userid=session["user"])
+
+    return render_template('multiple_choice.html', title="Create Tracker", username=user.fname)
 
 @app.route("/logout")
 def logout():
--- application/models.py
+++ application/models.py
@@ -9,15 +9,21 @@ class User(db.Model):
     lname = db.Column(db.String)
     tracker = db.relationship('Tracker', cascade="all, delete") # one-to-many relationship with delete on cascade
 
-class Tracker_type(db.Model):
-    __tablename__ = 'tracker_type'
-    typeid = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    type = db.Column(db.String, unique=True, nullable=False)
+#class Tracker_type(db.Model):
+  #  __tablename__ = 'tracker_type'
+  #  typeid = db.Column(db.Integer, autoincrement=True, primary_key=True)
+  #  type = db.Column(db.String, unique=True, nullable=False)
 
 class Tracker(db.Model):
     __tablename__ = 'tracker'
     trackerid = db.Column(db.Integer, autoincrement=True, primary_key=True)
     trackername = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String)
-    typeid = db.Column(db.Integer, db.ForeignKey("tracker_type.typeid"), nullable=False)
+    type = db.Column(db.String, nullable=False)
     userid = db.Column(db.Integer, db.ForeignKey("user.userid"), nullable=False)
+
+class MultipleChoice(db.Model):
+    __tablename__ = 'multiple_choice'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    trackerid = db.Column(db.Integer, db.ForeignKey("tracker.trackerid"), nullable=False)
+    choices = db.Column(db.String, nullable=False)
